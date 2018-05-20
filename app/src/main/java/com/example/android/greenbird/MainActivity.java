9a2bd428d723cfb8c66f94b6772c667174817e56
CarinaSilvaCunha <aniracsilva@gmail.com>
2018-05-20 12:37:41 +0100

    Defined main structure of the app

--- app/src/main/java/com/example/android/greenbird/MainActivity.java
+++ app/src/main/java/com/example/android/greenbird/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.greenbird;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
